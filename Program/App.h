7cf84228afb642767d702d91fc7ecad7ff85850c
Dzeferson <108546929+DzefersonS@users.noreply.github.com>
2023-12-17 17:51:38 +0200

    Added employee and pilot registration

--- Program/App.h
+++ Program/App.h
@@ -12,10 +12,11 @@ void doMainMenuLoop();
 void repaintStartMenu();
 void PerformCreateMenuLoop();
 void PerformUpdateMenuLoop();
-void PerformDeleteMenuLoop();
+void redrawDeleteMenuLoop();
 int linux_getch();
 int linux_kbhit();
 void printError();
+void verifyOperationSuccess();
 
 int linux_getch() {
     struct termios oldt, newt;
@@ -51,23 +52,4 @@ int linux_kbhit() {
     }
 
     return 0;
-}
-
-void printError()
-{
-	if(DEBUG_MODE)
-	{
-		fprintf(stderr, "==== sqlca ====\n");
-		fprintf(stderr, "sqlcode: %ld\n", sqlca.sqlcode);
-		fprintf(stderr, "sqlerrm.sqlerrml: %d\n", sqlca.sqlerrm.sqlerrml);
-		fprintf(stderr, "sqlerrm.sqlerrmc: %s\n", sqlca.sqlerrm.sqlerrmc);
-		fprintf(stderr, "sqlerrd: %ld %ld %ld %ld %ld %ld\n", sqlca.sqlerrd[0],sqlca.sqlerrd[1],sqlca.sqlerrd[2],
-															sqlca.sqlerrd[3],sqlca.sqlerrd[4],sqlca.sqlerrd[5]);
-		fprintf(stderr, "sqlwarn: %d %d %d %d %d %d %d %d\n", sqlca.sqlwarn[0], sqlca.sqlwarn[1], sqlca.sqlwarn[2],
-															sqlca.sqlwarn[3], sqlca.sqlwarn[4], sqlca.sqlwarn[5],
-															sqlca.sqlwarn[6], sqlca.sqlwarn[7]);
-		fprintf(stderr, "sqlstate: %5s\n", sqlca.sqlstate);
-		fprintf(stderr, "===============\n");
-		exit(-1);
-	}
 }
